b4e0adb9f384ef814cb1ac61e3aed8d0c367aeb4
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2023-02-08 00:30:56 -0300

    fixing bugs

--- primera-entrega.js
+++ primera-entrega.js
@@ -4,6 +4,21 @@ class ProductManager {
   }
   addProduct(title, description, price, thumbnail, code, stock) {
     const newProduct = { title, description, price, thumbnail, code, stock }
+
+    if (this.products.find(prod => prod.code === code)) {
+      console.log(`The code ${code} it's already used in the database`)
+    } else if (
+      !title ||
+      !description ||
+      !price ||
+      !thumbnail ||
+      !code ||
+      !stock
+    ) {
+      console.log('All fields are required')
+      return
+    }
+
     if (this.products.length > 0) {
       newProduct.id = [...this.products].pop().id + 1
     } else {
@@ -40,10 +55,3 @@ products.addProduct('rule', 'lorem ipsum', 'rule.svg', 3.55, 'A2', 50)
 console.log('3', products.getProducts())
 
 products.getProductById(3)
-
-// title (nombre del producto)
-// description (descripción del producto)
-// price (precio)
-// thumbnail (ruta de imagen)
-// code (código identificador)
-// stock (número de piezas disponibles)
