fd95232b129f59aed4026670517091a9806ef519
Gonzalo Dos Santos <die.zaubernacht@gmail.com>
2023-02-08 00:21:23 -0300

    fixing bugs

--- primera-entrega.js
+++ primera-entrega.js
@@ -1,55 +1,45 @@
-class Product {
-  constructor(title, description, price, thumbnail, code, stock) {
-    this.title = title
-    this.description = description
-    this.price = price
-    this.thumbnail = thumbnail
-    this.code = code
-    this.stock = stock
-  }
-}
-
 class ProductManager {
   constructor() {
     this.products = []
   }
-  addProduct(product) {
+  addProduct(title, description, price, thumbnail, code, stock) {
+    const newProduct = { title, description, price, thumbnail, code, stock }
     if (this.products.length > 0) {
-      product.id = this.products.pop().id + 1
+      newProduct.id = [...this.products].pop().id + 1
     } else {
-      product.id = 1
+      newProduct.id = 1
     }
-    this.products.push(product)
+    this.products.push(newProduct)
   }
+
   getProducts() {
     return this.products
+  }
+  getProductById(id) {
+    if (this.products.find(prod => prod.id === id)) {
+      console.log(
+        'Requested product:',
+        this.products.find(prod => prod.id === id)
+      )
+    } else {
+      console.log('Product not found')
+    }
   }
 }
 
 const products = new ProductManager()
 
-const product1 = new Product(
-  'pencil',
-  'lorem ipsum',
-  'pencil.svg',
-  10,
-  'A1',
-  100
-)
-
-const product2 = new Product('rule', 'lorem ipsum', 'rule.svg', 3.55, 'A2', 50)
-
 console.log('1', products.getProducts())
 
-products.addProduct(product1)
+products.addProduct('pencil', 'lorem ipsum', 'pencil.svg', 10, 'A1', 100)
 
 console.log('2', products.getProducts())
 
-products.addProduct(product2)
+products.addProduct('rule', 'lorem ipsum', 'rule.svg', 3.55, 'A2', 50)
 
 console.log('3', products.getProducts())
 
-console.log(products.getProducts.length)
+products.getProductById(3)
 
 // title (nombre del producto)
 // description (descripción del producto)
